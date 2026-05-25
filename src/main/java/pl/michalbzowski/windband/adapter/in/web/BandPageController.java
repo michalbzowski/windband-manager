dfe2d6ebc369a4348b596f4c4310849af53fb78a
Michał Bzowski <michal.bzowski@gmail.com>
2026-05-25 09:08:58 +0200

    fix: remove duplicate /band/attributes mapping in BandPageController
    BandPageController.attributeDefsPage() duplicated the GET /band/attributes
    mapping already handled by MemberAttributeController.attributesPage().
    Removed the duplicate method to fix AmbiguousMappingException on startup.
    
    Also adds AttributeUiTest.shouldAddAttributeAndAppearInList() E2E test.

--- src/main/java/pl/michalbzowski/windband/adapter/in/web/BandPageController.java
+++ src/main/java/pl/michalbzowski/windband/adapter/in/web/BandPageController.java
@@ -1,15 +1,9 @@
 package pl.michalbzowski.windband.adapter.in.web;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/band")
 public class BandPageController {
-
-    @GetMapping("/attributes")
-    public String attributeDefsPage() {
-        return "band/attribute-defs";
-    }
 }
