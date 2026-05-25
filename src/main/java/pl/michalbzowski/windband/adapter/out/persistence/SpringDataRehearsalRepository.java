bc34797ee51c268bf8731a53027ae0c219bd749e
Michał Bzowski <michal.bzowski@gmail.com>
2026-05-25 13:03:22 +0200

    fix: JOIN FETCH attendances w RehearsalRepository żeby uniknąć LazyInitializationException

--- src/main/java/pl/michalbzowski/windband/adapter/out/persistence/SpringDataRehearsalRepository.java
+++ src/main/java/pl/michalbzowski/windband/adapter/out/persistence/SpringDataRehearsalRepository.java
@@ -1,6 +1,7 @@
 package pl.michalbzowski.windband.adapter.out.persistence;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import pl.michalbzowski.windband.domain.rehearsal.Rehearsal;
 
 import java.time.LocalDate;
@@ -8,7 +9,9 @@ import java.util.List;
 
 public interface SpringDataRehearsalRepository extends JpaRepository<Rehearsal, Long> {
 
+    @Query("SELECT r FROM Rehearsal r LEFT JOIN FETCH r.attendances WHERE r.date BETWEEN :from AND :to ORDER BY r.date DESC")
     List<Rehearsal> findByDateBetweenOrderByDateDesc(LocalDate from, LocalDate to);
 
+    @Query("SELECT DISTINCT r FROM Rehearsal r LEFT JOIN FETCH r.attendances ORDER BY r.date DESC")
     List<Rehearsal> findAllByOrderByDateDesc();
 }
