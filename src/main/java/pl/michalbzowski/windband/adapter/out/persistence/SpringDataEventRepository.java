68ac933f3e91a6bfdf472e371a24908b957a1251
Michał Bzowski <michal.bzowski@gmail.com>
2026-05-25 13:50:51 +0200

    fix: JOIN FETCH participations w EventRepository żeby uniknąć LazyInitializationException

--- src/main/java/pl/michalbzowski/windband/adapter/out/persistence/SpringDataEventRepository.java
+++ src/main/java/pl/michalbzowski/windband/adapter/out/persistence/SpringDataEventRepository.java
@@ -1,6 +1,7 @@
 package pl.michalbzowski.windband.adapter.out.persistence;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import pl.michalbzowski.windband.domain.event.BandEvent;
 
 import java.time.LocalDate;
@@ -8,7 +9,9 @@ import java.util.List;
 
 public interface SpringDataEventRepository extends JpaRepository<BandEvent, Long> {
 
+    @Query("SELECT e FROM BandEvent e LEFT JOIN FETCH e.participations WHERE e.date BETWEEN :from AND :to ORDER BY e.date DESC")
     List<BandEvent> findByDateBetweenOrderByDateDesc(LocalDate from, LocalDate to);
 
+    @Query("SELECT DISTINCT e FROM BandEvent e LEFT JOIN FETCH e.participations ORDER BY e.date DESC")
     List<BandEvent> findAllByOrderByDateDesc();
 }
