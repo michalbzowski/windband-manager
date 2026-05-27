effcd8f6f50f2d2fa2b46749bdf101296d94583b
Michał Bzowski <michal.bzowski@gmail.com>
2026-05-27 18:04:20 +0200

    chore: remove debug logging from MemberAttributeController

--- src/main/java/pl/michalbzowski/windband/adapter/in/web/MemberAttributeController.java
+++ src/main/java/pl/michalbzowski/windband/adapter/in/web/MemberAttributeController.java
@@ -60,7 +60,6 @@ public class MemberAttributeController {
 
     @PostMapping
     public ResponseEntity<Void> createAttribute(@ModelAttribute AttributeDefForm form) {
-        System.out.println("DEBUG createAttribute: name=" + form.getName() + " type=" + form.getType() + " options=" + form.getOptions() + " required=" + form.isRequired() + " displayOrder=" + form.getDisplayOrder());
         Band band = bandRepository.findById(1L)
                 .orElseThrow(() -> new IllegalArgumentException("Band not found: 1"));
         commandService.createAttributeDef(band, form.getName(), form.getType(), form.isRequired(), form.getDisplayOrder(), form.getOptions());
@@ -69,7 +68,6 @@ public class MemberAttributeController {
 
     @PutMapping("/{id}")
     public ResponseEntity<Void> updateAttribute(@PathVariable Long id, @ModelAttribute AttributeDefForm form) {
-        System.out.println("DEBUG updateAttribute: id=" + id + " name=" + form.getName() + " type=" + form.getType() + " options=" + form.getOptions() + " required=" + form.isRequired() + " displayOrder=" + form.getDisplayOrder());
         commandService.updateAttributeDef(id, form.getName(), form.getType(), form.isRequired(), form.getDisplayOrder(), form.getOptions());
         return ResponseEntity.ok().header("HX-Redirect", "/band/attributes").build();
     }
