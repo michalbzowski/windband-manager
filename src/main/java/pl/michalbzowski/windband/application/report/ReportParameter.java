64c3003aab509fa50d0d8f0e4dae1129f4e306ca
Michał Bzowski <michal.bzowski@gmail.com>
2026-07-31 21:59:21 +0200

    fix(jasper-reports): Clean checkstyle violations - trailing whitespace and formatting

--- src/main/java/pl/michalbzowski/windband/application/report/ReportParameter.java
+++ src/main/java/pl/michalbzowski/windband/application/report/ReportParameter.java
@@ -16,21 +16,21 @@ public class ReportParameter {
 
     /** Nazwa parametru */
     private String name;
-    
+
     /** Typ Java (np. "java.lang.String", "java.util.Date") */
     @Builder.Default
     private String className = "java.lang.String";
-    
+
     /** Czy parametr ma być wyświetlany na formularzu użytkownika
      * forPrompting="false" oznacza hidden input z wartością domyślną */
     @Builder.Default
     private boolean forPrompting = true;
-    
+
     /** Typ wyświetlany w UI (np. "date", "text", "number") */
     public String getInputType() {
         if ("java.util.Date".equals(className) || "java.sql.Date".equals(className)) {
             return "date";
-        } else if ("java.lang.Integer".equals(className) 
+        } else if ("java.lang.Integer".equals(className)
                 || "java.lang.Long".equals(className)) {
             return "number";
         } else {
